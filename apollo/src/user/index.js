4008f0b5a4838c65af0a7b6b14e4ba0cafa000cf
RyChrome <dilsy3@gmail.com>
2019-06-15 14:56:38 -0400

    Added boilerplate for index.js

---
+++ apollo/src/user/index.js
@@ -0,0 +1,9 @@
+const { resolvers } = require('./resolvers')
+const { schema } = require('./schema')
+const { UserAPI } = require('./UserAPI')
+
+module.exports = {
+	resolvers,
+	schema,
+	UserAPI,
+}
