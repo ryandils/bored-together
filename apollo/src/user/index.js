29d9143ca96bf59de02d54b4bfd8613b52f73315
Ryan Thomas <ryanthomas30@gmail.com>
2019-06-15 20:47:58 -0400

    Merge conflict fix

--- apollo/src/user/index.js
+++ apollo/src/user/index.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
 export * from './schema'
-=======
-const { resolvers } = require('./resolvers')
-const { schema } = require('./schema')
-const { UserAPI } = require('./UserAPI')
-
-module.exports = {
-	resolvers,
-	schema,
-	UserAPI,
-}
->>>>>>> develop
