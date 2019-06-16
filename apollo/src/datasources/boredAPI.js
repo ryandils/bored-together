046be3da94d59f69bc047115be7ad6774d9b354a
Ryan Thomas <ryanthomas30@gmail.com>
2019-06-16 02:27:19 -0400

    Quick fix

--- apollo/src/datasources/boredAPI.js
+++ apollo/src/datasources/boredAPI.js
@@ -1,12 +1,12 @@
-const _encodeObject = (params) => Object.keys(params).map(key => key + '=' + params[key]).join('&')
+// const _encodeObject = (params) => Object.keys(params).map(key => key + '=' + params[key]).join('&')
+import axios from 'axios'
 
 /* Generates transient activity list of which one is launched */
 const generateActivities = async (count = 4) => {
 	const baseUrl = 'http://www.boredapi.com/api/activity?'
-	// const params = encodeObject(filters)
 	let activities = []
 	for (let i = 0; i < count; i++) {
-		const resp = await axios.get(`${baseUrl}${params}`)
+		const resp = await axios.get(`${baseUrl}`)
 		const data = resp.data
 		data.name = data.activity
 		data.activityType = data.type
